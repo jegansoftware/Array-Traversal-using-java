76f4f282724f8205806fcbb29afe763086495845
Jegan <83541158+jegansoftware@users.noreply.github.com>
2021-07-04 19:47:37 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+package Prime;
+
+public class ArrayTravesal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ArrayTravesala in=new ArrayTravesala();
+		in.arrayTraversal();
+	
+	}
+	
+
+}
